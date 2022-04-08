43c66b80244f8f1b5f4e4c1e6f6c16b3133f85bb
Joni Savolainen <joni@savolainen.io>
2022-04-08 18:33:59 +0300

    Export Logger in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import ARGV from "./argv";
 import parallelCmd, { parseParallelCmdOptionsFromArgv } from "./parallelCmd";
+import { Logger } from "./log";
 
-export { parallelCmd, ARGV, parseParallelCmdOptionsFromArgv };
+export { parallelCmd, ARGV, parseParallelCmdOptionsFromArgv, Logger };
